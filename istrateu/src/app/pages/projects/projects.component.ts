24a03acbb1a1fa853bb73c2b5fab358b979084c3
Maria Istrate <mistrate06@gmail.com>
2025-09-05 22:30:39 +0300

    portfolio page done

--- istrateu/src/app/pages/projects/projects.component.ts
+++ istrateu/src/app/pages/projects/projects.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CategoryCardComponent } from "../../components/category-card/category-card.component";
-
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-projects',
-  imports: [CategoryCardComponent],
+  imports: [CategoryCardComponent, MatIconModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
 
 }
+
